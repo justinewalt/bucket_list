a303c070e081cd588699dbea352c7316443c2cd2
Justin Ewalt <justinewalt@gmail.com>
2016-03-08 16:30:42 -0700

    built out the activities pages

--- Bucket_list/app/controllers/activities_controller.rb
+++ Bucket_list/app/controllers/activities_controller.rb
@@ -1,7 +1,47 @@
 class ActivitiesController < ApplicationController
-  def new
-  end
+
 
   def edit
   end
+
+  def update
+  	if @activities.update(activities_params)
+  		redirect_to buckets_path(@category, @activities)
+  	else
+  		render :edit
+  	end
+  end
+
+  def new
+  	@activities = Activity.new
+  end
+
+  def create
+  	@activities = @category.activities.new(activities_params)
+  	if @activities.save
+  		redirect_to buckets_path(@category)
+  	else
+  		render :new
+  	end
+  end
+
+  def destroy
+  	@activities.destroy
+  	redirect_to buckets_path(@category)
+  end
+
+  private
+
+  	def activities_params
+  		params.require(:activities).permit(:name)
+  	end
+
+  	def category
+  		@category = Category.find(params[:category_id])
+  	end
+
+  	def activities
+  		@activities = @category.activities.find(params[:id])
+  	end
+
 end
--- Bucket_list/app/controllers/buckets_controller.rb
+++ Bucket_list/app/controllers/buckets_controller.rb
@@ -1,4 +1,7 @@
 class BucketsController < ApplicationController
+	def index
+  	flash[:sucess] = "You have been signed in"
+  	
   def index
   end
 
