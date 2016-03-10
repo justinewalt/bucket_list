eace853619fe76e746eed8fea59bc3a1e9bff1c5
Karson Hatch <karsonhatch@gmail.com>
2016-03-10 16:02:44 -0700

    styles, yo

--- Bucket_list/db/migrate/20160308212807_create_buckets.rb
+++ Bucket_list/db/migrate/20160308212807_create_buckets.rb
@@ -2,7 +2,7 @@ class CreateBuckets < ActiveRecord::Migration
   def change
     create_table :buckets do |t|
       t.string :category
-      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true 
 
       t.timestamps null: false
     end
